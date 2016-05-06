cce77ad2a4d7cdcd7f588b9054e7bcb8b350b1f8
Mark Tompkins <mets_dev@outlook.com>
2016-05-06 17:34:32 -0400

    Permission changes

--- compile_linux_kernel.sh
+++ compile_linux_kernel.sh
@@ -12,6 +12,8 @@ clear
 # Source functions
 . ./functions
 
+chk_sudoer
+
 # Set overlap variables
 DEPENDENCIES="gcc make fakeroot libncurses5 libncurses5-dev kernel-package \
 						build-essential pkg-config qt5-qmake libnotify-bin \
